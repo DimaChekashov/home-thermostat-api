55e3b42ab51e6c51c9ffad73537298daf661f80d
DimaChekashov <foxdima99@gmail.com>
2026-01-31 19:06:49 +0300

    Prepare project

---
+++ src/main/java/com/example/home_thermostat_api/HomeThermostatApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_thermostat_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeThermostatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeThermostatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/home_thermostat_api/HomeThermostatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_thermostat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeThermostatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
